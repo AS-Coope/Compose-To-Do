a561f6e4060b16782659b613359226fa2a3c14b5
AS-Coope <deltazirconium@gmail.com>
2024-06-05 22:28:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/composetodo/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.composetodo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composetodo.ui.theme.ComposeToDoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeToDoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeToDoTheme {
+        Greeting("Android")
+    }
+}
+
+// First version of To Do app:
+/*
+Allow the user to enter input in text input
+Allow the user to add that to the list
+Allow the user to check that item as complete from the list
+Don't have to worry about persisting data upon app close or even on onPause()
+ */
